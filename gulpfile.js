60e3c9ffa384b1c38849b8ac01004d44049165c8
Dipu Raj <dipumedayil@gmail.com>
2020-02-03 16:17:24 +0530

    Build setup upgraded

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,118 @@
-var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    babel = require("gulp-babel"),
-    postcss = require('gulp-postcss'),
-    cleanCSS = require('gulp-clean-css'),
-    cssbeautify = require('gulp-cssbeautify'),
-    autoprefixer = require('autoprefixer'),
-    karma = require('karma'),
-    del = require('del');
+var gulp          = require('gulp'),
+    jshint        = require('gulp-jshint'),
+    rename        = require('gulp-rename'),
+    uglify        = require('gulp-uglify'),
+    saveLicense   = require('uglify-save-license'),
+    babel         = require("gulp-babel"),
+    postcss       = require('gulp-postcss'),
+    cleanCSS      = require('gulp-clean-css'),
+    cssbeautify   = require('gulp-cssbeautify'),
+    autoprefixer  = require('autoprefixer'),
+    del           = require('del');
+
+var Server        = require('karma').Server;
 
 // Source files
-var SRC_JS = 'src/js/*.js';
-var SRC_CSS = 'src/css/*.css';
+var SRC_JS        = 'src/js/*.js';
+var SRC_CSS       = 'src/css/*.css';
 
 // Destination folders
-var DEST_JS = 'dist/js';
-var DEST_CSS = 'dist/css';
+var DEST_JS       = 'dist/js';
+var DEST_CSS      = 'dist/css';
 
-// JS TASKS
-// Lint JS
-gulp.task('lint:js', function() {
-    return gulp.src(SRC_JS)
-            .pipe(jshint())
-            .pipe(jshint.reporter('default'))
-            .pipe(jshint.reporter('fail'));
-});
 
-// Build JS
-gulp.task('build:js', ['clean:js', 'lint:js'], function() {
-    return gulp.src(SRC_JS)
-            .pipe(babel())
-            .pipe(gulp.dest(DEST_JS))
-            .pipe(uglify({preserveComments:'license'}))
-            .pipe(rename({suffix: '.min'}))
-            .pipe(gulp.dest(DEST_JS));
-});
+// BUILD
+const build_js = gulp.series(clean_js, lint_js, function(cb) {
+                      gulp.src(SRC_JS)
+                            .pipe(babel())
+                            .pipe(gulp.dest(DEST_JS))
+                            .pipe(uglify({
+                                output: {
+                                    comments: saveLicense
+                                }
+                            }))
+                            .pipe(rename({
+                                suffix: '.min'
+                            }))
+                            .pipe(gulp.dest(DEST_JS));
 
-// CSS TASKS
-gulp.task('build:css', ['clean:css'], function () {
-    return gulp.src(SRC_CSS)
-            .pipe(postcss( [autoprefixer({browsers: ['last 10 versions']})] ))
-            .pipe(cssbeautify({ autosemicolon: true }))
-            .pipe(gulp.dest(DEST_CSS))
-            .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(rename({suffix: '.min'}))
-            .pipe(gulp.dest(DEST_CSS));
-});
+                      cb();
+                    });
 
-// CLEAN files
-gulp.task('clean', function () {
-    gulp.start( 'clean:js', 'clean:css');
-});
+const build_css = gulp.series(clean_css, function(cb) {
+                      gulp.src(SRC_CSS)
+                            .pipe(postcss( [autoprefixer()] ))
+                            .pipe(cssbeautify({ autosemicolon: true }))
+                            .pipe(gulp.dest(DEST_CSS))
+                            .pipe(cleanCSS({compatibility: 'ie8'}))
+                            .pipe(rename({suffix: '.min'}))
+                            .pipe(gulp.dest(DEST_CSS));
 
-gulp.task('clean:js', function () {
-    return del([DEST_JS]);
-});
+                      cb();
+                    });
 
-gulp.task('clean:css', function () {
-    return del([DEST_CSS]);
-});
+const build = gulp.parallel(build_js, build_css);
+build.description = 'Build the files';
 
-// WATCH for file changes and rerun the task
-gulp.task('watch', function() {
-    gulp.watch(SRC_JS, ['build:js']);
-    gulp.watch(SRC_CSS, ['build:css']);
-});
+// LINT
+const lint = gulp.parallel(lint_js);
+lint.description = 'Link js files';
+
+function lint_js(cb) {
+  gulp.src(SRC_JS)
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'))
+      .pipe(jshint.reporter('fail'));
+
+  cb();
+}
+
+
+// CLEAN
+const clean = gulp.parallel(clean_js, clean_css);
+clean.description = 'Clean the files'
+
+function clean_js(cb) {
+  del([DEST_JS]);
+
+  cb();
+}
+
+function clean_css(cb) {
+  del([DEST_CSS]);
+
+  cb();
+}
+
+
+// WATCH
+const watch = gulp.parallel(watch_all);
+watch.description = 'Watch for changes to all source';
+
+function watch_all(cb) {
+  gulp.watch(SRC_JS, build_js);
+  gulp.watch(SRC_CSS, build_css);
+
+  cb();
+}
+
+
 
 // TEST
-gulp.task('test', function (done) {
-    new karma.Server({
-      configFile: __dirname + '/karma.conf.js',
-      singleRun: true
-    }, function() {
-        done();
-    }).start();
-});
+function test(cb) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
 
-// DEFAULT task
-gulp.task('default', function() {
-    gulp.start( 'build:js', 'build:css' );
-});
+  cb();
+}
+
+
+// EXPORT methods
+exports.clean   = clean;
+exports.build   = build;
+exports.lint    = lint;
+exports.watch   = watch;
+exports.test    = test;
+exports.default = exports.build;
