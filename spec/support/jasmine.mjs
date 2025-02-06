678f1c68289200d24f7fb4665b7601b4f337b398
Dipu Rajendran <dipumedayil@gmail.com>
2025-02-06 16:54:19 +0530

    Test runners

---
+++ spec/support/jasmine.mjs
@@ -0,0 +1,14 @@
+export default {
+  spec_dir: "test",
+  spec_files: [
+    "**/*[sS]pec.?(m)js"
+  ],
+  helpers: [
+    "helpers/**/*.?(m)js"
+  ],
+  env: {
+    stopSpecOnExpectationFailure: false,
+    random: true,
+    forbidDuplicateNames: true
+  }
+}
